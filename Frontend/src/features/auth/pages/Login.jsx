c443263899793db33f8e9e2eb8958950aaf244fa
luvpee <luvpeezz@gmail.com>
2026-08-26 16:47:34 +0530

    Improved UI for authentication

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import "../auth.form.scss"
+import {useNavigate,Link} from 'react-router'
 
 const Login =() => {
+  const navigate=useNavigate();
+
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" name='email' placeholder='Enter email address'/>
@@ -16,6 +23,7 @@ const Login =() => {
           </div>
           <button className='button primary-button'>Login</button>
         </form>
+        <p>Don't have an account? <Link to={"/register"}>Register</Link></p>
 
       </div>
     </main>
---
+++ Frontend/src/features/auth/services/auth.api.js
@@ -0,0 +1,6 @@
+// import axios from "axios"
+
+
+// export async function register({username,email,password}){
+//   axios.post()
+// }
